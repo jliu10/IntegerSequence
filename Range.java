f18731c330f76ae141b1fc5cd9b45c5575ff7464
jliu10 <jliu10@stuy.edu>
2020-12-05 16:50:15 -0500

    modified constructor to work with original variable names

--- Range.java
+++ Range.java
@@ -2,9 +2,9 @@ import java.util.NoSuchElementException;
 public class Range implements IntegerSequence{
   private int start,end,current;
 
-  public Range(int s, int e){
-    start=s;
-    end=e;
+  public Range(int start, int end){
+    this.start=start;
+    this.end=end;
     current=start;
   }
 
