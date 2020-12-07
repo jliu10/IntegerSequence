1835bc662fe46b1c943228d02920567bf3aca0a7
jliu10 <jliu10@stuy.edu>
2020-12-07 14:14:17 -0500

    wrote constructor that takes array; wrote interface methods

--- ArraySequence.java
+++ ArraySequence.java
@@ -5,10 +5,37 @@ public class ArraySequence implements IntegerSequence{
   private int []data;
 
   /*Construct the sequence by copying values from the other array into the data array*/
-  public ArraySequence(int [] other){  }
+  public ArraySequence(int [] other){
+    currentIndex=0;
+    data=new int[other.length];
+    //for(int n: other){
+    for(int i=0;i<other.length;i++){
+      data[i]=other[i];
+    }
+  }
 
 
-//throw a NoSuchElementException
+  public boolean hasNext(){//does the sequence have more elements?
+    return currentIndex<length();
+  }
+
+  //throw a NoSuchElementException
+  public int next(){       //return the current value in the sequence and advances to the next element.
+    if(!hasNext()){
+      throw new NoSuchElementException("No more values left");
+    }
+    int c=data[currentIndex];
+    currentIndex++;
+    return c;
+  }
+
+  public int length(){     //returns the total length of the sequence
+    return data.length;
+  }
+
+  public void reset(){     //start over from the start of the sequence
+    currentIndex=0;
+  }
 
 //Postcondition: The otherseq will be reset.
 //This constructor will copy ALL values of the `otherseq` into the data array.
@@ -17,8 +44,15 @@ public class ArraySequence implements IntegerSequence{
   public static void main(String[] args){
 
     try{
+      int[]nums = {1,3,5,0,-1,3,9};
+      IntegerSequence as = new ArraySequence(nums);
 
-
+      System.out.println("ArraySequence(array):");
+      while(as.hasNext()){
+        System.out.print(as.next()+", ");
+      }
+      System.out.println();
+      
     }catch(NoSuchElementException e){
       System.out.println("No more values left");
     }
